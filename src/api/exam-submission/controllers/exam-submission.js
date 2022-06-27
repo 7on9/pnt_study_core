8a0acdb279b799720abfce87c1f65a04c3bfe47d
Long Nguyen <longnt98.dev@gmail.com>
2022-06-28 02:08:07 +0700

    add model

---
+++ src/api/exam-submission/controllers/exam-submission.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  exam-submission controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::exam-submission.exam-submission');
---
+++ src/api/exam-submission/routes/exam-submission.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * exam-submission router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::exam-submission.exam-submission');
